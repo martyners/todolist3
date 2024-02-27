f7b9bdab3296645199dc08f406459143d4adb1cc
Martyna MH <martyna.michalak@gmail.com>
2024-02-27 19:49:23 +0100

    new task added

--- js/script.js
+++ js/script.js
@@ -24,6 +24,22 @@
     
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+            tasks.push({
+                content: newTaskContent,
+            done: false
+            });
+            render();
+        })
     };
 
     init();
