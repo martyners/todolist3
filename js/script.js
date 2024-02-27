a03b65c0e534b168a049608b85667f7578b7c535
Martyna MH <martyna.michalak@gmail.com>
2024-02-27 19:27:24 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+{
+    const tasks = [
+        {
+            content: 'zadanie1',
+            done: 'false',
+        },
+        {
+            content: 'zadanie2',
+            done: 'true',
+        },
+    ];
+
+    const render = () => {
+        let htmlString = '';
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+                ${task.content}
+            </li>
+            `
+        };
+        document.querySelector('.js-tasks').innerHTML = htmlString;
+    }
+    
+    const init = () => {
+        render();
+    };
+
+    init();
+};
