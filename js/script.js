a0cb68b5f7de7c8b23ed70b2a31d10f929a32629
Martyna MH <martyna.michalak@gmail.com>
2024-02-27 20:17:49 +0100

    delete task added

--- js/script.js
+++ js/script.js
@@ -9,37 +9,54 @@
             done: true,
         },
     ];
+    
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+            done: false,
+        });
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
 
     const render = () => {
         let htmlString = '';
         for (const task of tasks) {
             htmlString += `
             <li${task.done ? ' class="list__listItem--done"' : ''}>
+                <button class="js-remove">usuń</button>
                 ${task.content}
             </li>
         `;
         };
         document.querySelector('.js-tasks').innerHTML = htmlString;
-    }
-    
-    const init = () => {
-        render();
 
-        const form = document.querySelector(".js-form");
-        form.addEventListener("submit", (event) => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", removeTask)
+        })
+    };
+
+    const onFormSubmit = (event) => {
             event.preventDefault();
-
             const newTaskContent = document.querySelector(".js-newTask").value.trim();
 
             if (newTaskContent === "") {
                 return;
             }
-            tasks.push({
-                content: newTaskContent,
-            done: false
-            });
-            render();
-        })
+            addNewTask(newTaskContent);
+    }
+    
+    
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit)
     };
 
     init();
